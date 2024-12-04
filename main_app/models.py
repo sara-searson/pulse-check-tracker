5749c57424d823c398b7650d2bc895e9e1a5306b
sara-searson <sara.r.searson@gmail.com>
2024-12-04 16:47:49 -0500

    functionality added, mods 4&5

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True, null=True)
+    start = models.DateField()
+    due = models.DateField(blank=True, null=True)
+    lang = models.CharField(max_length=50, blank=True, null=True)
+    STATUS_CHOICES = [
+        ('Ideation', 'Ideation'),
+        ('Planned', 'Planned'),
+        ('In Progress', 'In Progress'),
+        ('On Hold', 'On Hold'),
+        ('Refactoring', 'Refactoring'),
+        ('Testing', 'Testing'),
+        ('Deployed', 'Deployed'),
+        ('Completed', 'Completed'),
+        ('Archived', 'Archived'),
+        ('Cancelled', 'Cancelled'),
+    ]
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
+    link = models.URLField(blank=True, null=True)
+    img = models.URLField(blank=True, null=True)
+
+def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,29 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+class Project:
+    def __init__(self, name, desc, start, lang, status, link):
+        self.name = name
+        self.desc = desc
+        self.start = start
+        self.lang = lang
+        self.status = status
+        self.link = link
+
+
+projects = [
+    Project('PoetrySim', 'Simple front end JS game', '2024-09-26', 'JavaScript', 'Needs Refactoring', 'https://github.com/sara-searson/PoetrySim,'),
+    Project('Ralphs Den', 'Video game collection management site', '2024-10-17', 'JavaScript', 'Needs Refactoring', 'https://github.com/sara-searson/ralphs-den'),
+    Project('Sprout', 'Self improvement/Hobby finder app', '2024-11-15', 'React', 'Portfolio Ready', 'https://github.com/siseldyke/self-learner-app-front-end'),
+    Project('Pulse Check', 'Project status management app', '2024-12-03', 'Django', 'In Progress', 'https://github.com/sara-searson/pulse-check-tracker'),
+]
+
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Hello Engineers!</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def project_index(request):
+    return render(request, 'projects/index.html', {'projects': projects})
